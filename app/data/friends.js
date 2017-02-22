0bdd139c198959dd8046bd7d5e105625dfc82c34
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2017-02-22 13:53:22 -0500

    working app, need to clean up or refactor some things

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,15 @@
-var friendsList = [];
+var friendsList = [{
+    name: 'Alex Rosenkranz',
+    picture: 'https://avatars0.githubusercontent.com/u/4238841?v=3&s=460',
+    scores: [4, 3, 4, 2, 4, 5, 3, 3, 5, 5]
+}, {
+    name: 'Bodie Rosenkranz',
+    picture: 'https://avatars0.githubusercontent.com/u/4238841?v=3&s=460',
+    scores: [5, 5, 1, 2, 5, 3, 1, 3, 4, 5]
+}, {
+    name: 'Rick Sanchez',
+    picture: 'http://vignette3.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png/revision/latest?cb=20160923150728',
+    scores: [5, 5, 5, 4, 5, 5, 5, 1, 1, 1]
+}];
 
 module.exports = friendsList;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,14 +11,35 @@ router.use(function timeLog(req, res, next) {
 
 router.post('/api/friends', function(req, res) {
     var newSurvey = req.body;
+    console.log(newSurvey);
+    let pickedFriend;
+    var friendCalc = [];
+    for (var i = 0; i < friendsList.length; i++) {
+        var totalDifference = 0;
+        for (var k = 0; k < 10; k++) {
+            let scoreDiff = Math.abs(friendsList[i].scores[k] - newSurvey.scores[k]);
+            totalDifference += scoreDiff;
+        }
+        console.log('Total Diff ' + i + ': ' + totalDifference);
+        friendCalc.push({
+            name: friendsList[i].name,
+            picture: friendsList[i].picture,
+            totalDiff: totalDifference
+        });
+    }
+    let maxScore = 40;
+    friendCalc.map(function(obj) {
+        if (obj.totalDiff < maxScore) maxScore = obj.totalDiff;
+    });
+    pickedFriend = friendCalc.filter(function(e) { return e.totalDiff == maxScore; });
+
+    res.json(pickedFriend);
     friendsList.push(newSurvey);
-    res.json(newSurvey);
-    console.log(friendsList);
 
 });
 
 router.get('/api/friends', function(req, res) {
-    res.send('Hi there friends!');
+    res.json(friendsList);
 });
 
 module.exports = router;
